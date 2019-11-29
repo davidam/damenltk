5c61694158d08afb6dc67d9d617c75bb7519378a
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-11-29 12:07:02 +0100

    setup.py: add readme in long_description

--- setup.py
+++ setup.py
@@ -22,15 +22,22 @@
 # Boston, MA 02110-1301 USA,
 
 from setuptools import setup
+from os import path
 
-def readme():
-    with open('README.org') as f:
-        return f.read()
+# def readme():
+#     with open('README.org') as f:
+#         return f.read()
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='damenltk',
-      version='0.0.4',
+      version='0.0.11',
       description='Learning about Natural Language Tool Kit (NLTK) from tests',
-      long_description=readme(),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
