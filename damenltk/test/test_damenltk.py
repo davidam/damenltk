5755c02f345ef038683b30e586f3bb6ed1468f83
David Arroyo Menéndez <davidam@gmail.com>
2020-11-28 05:29:17 +0100

    test/test_damenltk.py: add remove_words_not_included_in_language

--- damenltk/test/test_damenltk.py
+++ damenltk/test/test_damenltk.py
@@ -44,6 +44,13 @@ class TddInPythonExample(unittest.TestCase):
         dn = DameNLTK()
         self.assertEqual(['For', 'sequences', ',', '(', 'strings', ',', 'lists', ',', 'tuples', ')', ',', 'use', 'fact', 'empty', 'sequences', 'false', '.'], dn.remove_stopwords("For sequences, (strings, lists, tuples), use the fact that empty sequences are false."))
 
+    def test_remove_words_not_included_in_language(self):
+        dn = DameNLTK()
+        sent = "Io andiamo to the beach with my amico."        
+        words = dn.remove_words_not_included_in_language(sent, "en")
+#        print(words)
+        self.assertEqual(words, ["Io", "to", "the", "beach", "with", "my"])
+        
     def test_stopwords_definition_method_returns_correct_result(self):
         st0 = stopwords.words('english')
         st1 = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
