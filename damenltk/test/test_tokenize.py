85d6f89d3a40d64d7de40d649c814ba1f271bfae
David Arroyo Menéndez <davidam@gmail.com>
2021-07-05 23:48:03 +0200

    test/test_tokenize.py: fixing PEP8

--- damenltk/test/test_tokenize.py
+++ damenltk/test/test_tokenize.py
@@ -24,15 +24,21 @@
 import unittest
 import nltk
 
+
 class TddInPythonExample(unittest.TestCase):
 
     def test_tokenize_method_returns_correct_result(self):
-        sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+        sentence = "At eight o'clock on Thursday morning "
+        sentence = sentence + "Arthur didn't feel very good."
         tokens = nltk.word_tokenize(sentence)
-        self.assertEqual(tokens, ['At', 'eight', "o'clock", 'on', 'Thursday', 'morning', 'Arthur', 'did', "n't", 'feel', 'very', 'good', '.'])
+        self.assertEqual(tokens,
+                         ['At', 'eight', "o'clock", 'on', 'Thursday',
+                          'morning', 'Arthur', 'did', "n't", 'feel',
+                          'very', 'good', '.'])
 
     def test_tagged_method_returns_correct_result(self):
-        sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+        sentence = "At eight o'clock on Thursday morning"
+        sentence = sentence + " Arthur didn't feel very good."
         tokens = nltk.word_tokenize(sentence)
         tagged = nltk.pos_tag(tokens)
         self.assertEqual(tagged[0:2], [('At', 'IN'), ('eight', 'CD')])
