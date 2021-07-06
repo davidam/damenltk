939e672189b1134858b8fa349b2dd87e57a6e27d
David Arroyo Menéndez <davidam@gmail.com>
2021-07-06 21:29:21 +0200

    src/syllable.py: fixing PEP8

--- damenltk/src/syllable.py
+++ damenltk/src/syllable.py
@@ -21,36 +21,43 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-#import nltk.corpus.cmudict  # this does not work!?!?
 from nltk.corpus import cmudict
 import string
 
+
 class Syllable(object):
     def syllables_in_word(self, word):
-        '''Attempts to count the number of syllables in the string argument 'word'.
-
-        Limitation: word must be in the CMU dictionary (but that was a premise of the Exercise)
-        "Algorithm": no. syllables == no. (0,1,2) digits in the dictionary entry, right??
-    '''
+        '''Attempts to count the number of syllables in
+        the string argument 'word'.
+        Limitation: word must be in the CMU dictionary
+        (but that was a premise of the Exercise)
+        "Algorithm": no. syllables == no. (0,1,2)
+        digits in the dictionary entry, right??
+        '''
         phoneme_dict = dict(cmudict.entries())
-        # although listcomps may be readable, you can't insert print statements to instrument them!!
+        # although listcomps may be readable,
+        # you can't insert print statements to instrument them!!
         if word in phoneme_dict:
-        #return sum([ phoneme.count(str(num)) for phoneme in phoneme_dict[word] for num in range(3) ])
-            return len( [ph for ph in phoneme_dict[word] if ph.strip(string.letters)] )   # more destructive; less efficient? NO! see timeit results in my comments below
+            # more destructive; less efficient?
+            # NO! see timeit results in my comments below
+            return len([ph for ph in phoneme_dict[word]
+                        if ph.strip(string.letters)])
         else:
             return 0
 
     def syllables_in_text(self, text):
-        '''Attempts to count the number of syllables in the string argument 'text'.
-
-        Limitation: any "internal punctuation" must be part of the word. (it wouldn't get "this,and" correctly)
+        '''Attempts to count the number of syllables
+        in the string argument 'text'.
+        Limitation: any "internal punctuation" must be part of the word.
+        (it wouldn't get "this,and" correctly)
         Lets syllables_in_word do the heavy lifting.
         '''
-
-        # ok, so apparently str.split(delim) only works for A SINGLE CHAR delim...
-        # anything fancier, and you might want a regex (and its associated performance penalty)
-        return sum([syllables_in_word(word.strip(string.punctuation))       # but str.strip(delims) will strip all leading and trailing chars in "delims"!
-            for word in text.split()])                              # - alternatives at http://stackoverflow.com/questions/265960/
-
-s = Syllable()
-print(s.syllables_in_word("hello"))
+        # ok, so apparently str.split(delim)
+        # only works for A SINGLE CHAR delim...
+        # anything fancier, and you might want a regex
+        # (and its associated performance penalty)
+        # but str.strip(delims) will strip all leading
+        # and trailing chars in "delims"!
+        # - alternatives at http://stackoverflow.com/questions/265960/
+        return sum([syllables_in_word(word.strip(string.punctuation))
+                    for word in text.split()])
