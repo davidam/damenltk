0961137e08f313e18241fc4412a6d8d2b3f77dac
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-08-26 15:43:52 -0300

    test/test_damenltk.py: more words in textes to guess spanish

--- damenltk/test/test_damenltk.py
+++ damenltk/test/test_damenltk.py
@@ -32,6 +32,27 @@ class TddInPythonExample(unittest.TestCase):
     def test_detectlanguage_spanish_method_returns_correct_result(self):
         dn = DameNLTK()
         textes = '''
+        En un lugar de la Mancha, de cuyo nombre no quiero acordarme,
+        no ha mucho tiempo que vivía un hidalgo de los de lanza en
+        astillero, adarga antigua, rocín flaco y galgo corredor.
+        Una olla de algo más vaca que carnero, salpicón las más
+        noches, duelos y quebrantos los sábados, lantejas los
+        viernes, algún palomino de añadidura los domingos, consumían
+        las tres partes de su hacienda. El resto della concluían sayo
+        de velarte, calzas de velludo para las fiestas, con sus
+        pantuflos de lo mesmo, y los días de entresemana se honraba
+        con su vellorí de lo más fino. Tenía en su casa una ama que
+        pasaba de los cuarenta, y una sobrina que no llegaba a los
+        veinte, y un mozo de campo y plaza, que así ensillaba el
+        rocín como tomaba la podadera. Frisaba la edad de nuestro
+        hidalgo con los cincuenta años; era de complexión recia,
+        seco de carnes, enjuto de rostro, gran madrugador y amigo de
+        la caza. Quieren decir que tenía el sobrenombre de Quijada, o
+        Quesada, que en esto hay alguna diferencia en los autores que
+        deste caso escriben; aunque, por conjeturas verosímiles, se
+        deja entender que se llamaba Quejana. Pero esto importa poco
+        a nuestro cuento; basta que en la narración dél no se salga un
+        punto de la verdad.
         En un lugar de la Mancha de cuyo nombre no quiero
         acordarme, vivía un ingenioso hidalgo
         '''
@@ -100,3 +121,4 @@ class TddInPythonExample(unittest.TestCase):
         st1 = ['de', 'la', 'que', 'el', 'en', 'y', 'a', 'los', 'del', 'se', 'las', 'por', 'un', 'para', 'con', 'no', 'una', 'su', 'al', 'lo', 'como', 'más', 'pero', 'sus', 'le', 'ya', 'o', 'este', 'sí', 'porque', 'esta', 'entre', 'cuando', 'muy', 'sin', 'sobre', 'también', 'me', 'hasta', 'hay', 'donde', 'quien', 'desde', 'todo', 'nos', 'durante', 'todos', 'uno', 'les', 'ni', 'contra', 'otros', 'ese', 'eso', 'ante', 'ellos', 'e', 'esto', 'mí', 'antes', 'algunos', 'qué', 'unos', 'yo', 'otro', 'otras', 'otra', 'él', 'tanto', 'esa', 'estos', 'mucho', 'quienes', 'nada', 'muchos', 'cual', 'poco', 'ella', 'estar', 'estas', 'algunas', 'algo', 'nosotros', 'mi', 'mis', 'tú', 'te', 'ti', 'tu', 'tus', 'ellas', 'nosotras', 'vosotros', 'vosotras', 'os', 'mío', 'mía', 'míos', 'mías', 'tuyo', 'tuya', 'tuyos', 'tuyas', 'suyo', 'suya', 'suyos', 'suyas', 'nuestro', 'nuestra', 'nuestros', 'nuestras', 'vuestro', 'vuestra', 'vuestros', 'vuestras', 'esos', 'esas', 'estoy', 'estás', 'está', 'estamos', 'estáis', 'están', 'esté', 'estés', 'estemos', 'estéis', 'estén', 'estaré', 'estarás', 'estará', 'estaremos', 'estaréis', 'estarán', 'estaría', 'estarías', 'estaríamos', 'estaríais', 'estarían', 'estaba', 'estabas', 'estábamos', 'estabais', 'estaban', 'estuve', 'estuviste', 'estuvo', 'estuvimos', 'estuvisteis', 'estuvieron', 'estuviera', 'estuvieras', 'estuviéramos', 'estuvierais', 'estuvieran', 'estuviese', 'estuvieses', 'estuviésemos', 'estuvieseis', 'estuviesen', 'estando', 'estado', 'estada', 'estados', 'estadas', 'estad', 'he', 'has', 'ha', 'hemos', 'habéis', 'han', 'haya', 'hayas', 'hayamos', 'hayáis', 'hayan', 'habré', 'habrás', 'habrá', 'habremos', 'habréis', 'habrán', 'habría', 'habrías', 'habríamos', 'habríais', 'habrían', 'había', 'habías', 'habíamos', 'habíais', 'habían', 'hube', 'hubiste', 'hubo', 'hubimos', 'hubisteis', 'hubieron', 'hubiera', 'hubieras', 'hubiéramos', 'hubierais', 'hubieran', 'hubiese', 'hubieses', 'hubiésemos', 'hubieseis', 'hubiesen', 'habiendo', 'habido', 'habida', 'habidos', 'habidas', 'soy', 'eres', 'es', 'somos', 'sois', 'son', 'sea', 'seas', 'seamos', 'seáis', 'sean', 'seré', 'serás', 'será', 'seremos', 'seréis', 'serán', 'sería', 'serías', 'seríamos', 'seríais', 'serían', 'era', 'eras', 'éramos', 'erais', 'eran', 'fui', 'fuiste', 'fue', 'fuimos', 'fuisteis', 'fueron', 'fuera', 'fueras', 'fuéramos', 'fuerais', 'fueran', 'fuese', 'fueses', 'fuésemos', 'fueseis', 'fuesen', 'sintiendo', 'sentido', 'sentida', 'sentidos', 'sentidas', 'siente', 'sentid', 'tengo', 'tienes', 'tiene', 'tenemos', 'tenéis', 'tienen', 'tenga', 'tengas', 'tengamos', 'tengáis', 'tengan', 'tendré', 'tendrás', 'tendrá', 'tendremos', 'tendréis', 'tendrán', 'tendría', 'tendrías', 'tendríamos', 'tendríais', 'tendrían', 'tenía', 'tenías', 'teníamos', 'teníais', 'tenían', 'tuve', 'tuviste', 'tuvo', 'tuvimos', 'tuvisteis', 'tuvieron', 'tuviera', 'tuvieras', 'tuviéramos', 'tuvierais', 'tuvieran', 'tuviese', 'tuvieses', 'tuviésemos', 'tuvieseis', 'tuviesen', 'teniendo', 'tenido', 'tenida', 'tenidos', 'tenidas', 'tened']
         self.assertEqual(st0[0], "de")
         self.assertEqual(st0[-1], "tened")
+        
